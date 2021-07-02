7a8be87d9e777a137ccd9b95ae24c7bb961c2182
MeerTheCoder <kanta-mir@users.noreply.github.com>
2021-07-02 13:27:25 +0200

    Add files via upload

---
+++ File Renamer/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace File_Renamer
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowseFolder = new System.Windows.Forms.Button();
+            this.btnRename = new System.Windows.Forms.Button();
+            this.txtFolderPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSourceText = new System.Windows.Forms.TextBox();
+            this.txtTargetText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnBrowseFolder
+            // 
+            this.btnBrowseFolder.Location = new System.Drawing.Point(648, 33);
+            this.btnBrowseFolder.Name = "btnBrowseFolder";
+            this.btnBrowseFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowseFolder.TabIndex = 0;
+            this.btnBrowseFolder.Text = "Browse";
+            this.btnBrowseFolder.UseVisualStyleBackColor = true;
+            this.btnBrowseFolder.Click += new System.EventHandler(this.btnBrowseFolder_Click);
+            // 
+            // btnRename
+            // 
+            this.btnRename.Location = new System.Drawing.Point(648, 127);
+            this.btnRename.Name = "btnRename";
+            this.btnRename.Size = new System.Drawing.Size(75, 60);
+            this.btnRename.TabIndex = 1;
+            this.btnRename.Text = "Rename";
+            this.btnRename.UseVisualStyleBackColor = true;
+            this.btnRename.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtFolderPath
+            // 
+            this.txtFolderPath.Location = new System.Drawing.Point(130, 33);
+            this.txtFolderPath.Name = "txtFolderPath";
+            this.txtFolderPath.Size = new System.Drawing.Size(497, 20);
+            this.txtFolderPath.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Folder Path";
+            // 
+            // txtSourceText
+            // 
+            this.txtSourceText.Location = new System.Drawing.Point(130, 60);
+            this.txtSourceText.Name = "txtSourceText";
+            this.txtSourceText.Size = new System.Drawing.Size(497, 20);
+            this.txtSourceText.TabIndex = 4;
+            // 
+            // txtTargetText
+            // 
+            this.txtTargetText.Location = new System.Drawing.Point(130, 86);
+            this.txtTargetText.Name = "txtTargetText";
+            this.txtTargetText.Size = new System.Drawing.Size(497, 20);
+            this.txtTargetText.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Replace This";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "With This";
+            // 
+            // frmMain
+            // 
+            this.AcceptButton = this.btnRename;
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(763, 214);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTargetText);
+            this.Controls.Add(this.txtSourceText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFolderPath);
+            this.Controls.Add(this.btnRename);
+            this.Controls.Add(this.btnBrowseFolder);
+            this.Name = "frmMain";
+            this.Text = "File Renamer 0.1";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.frmMain_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.frmMain_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowseFolder;
+        private System.Windows.Forms.Button btnRename;
+        private System.Windows.Forms.TextBox txtFolderPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSourceText;
+        private System.Windows.Forms.TextBox txtTargetText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ File Renamer/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace File_Renamer
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var folderPath = txtFolderPath.Text;
+            var replaceThis = txtSourceText.Text;
+            var withThis = txtTargetText.Text;
+
+            DirectoryInfo d = new DirectoryInfo(folderPath);
+            FileInfo[] infos = d.GetFiles();
+
+            foreach (FileInfo f in infos)
+            {
+                try
+                {
+                    File.Move(f.FullName, f.FullName.Replace(replaceThis, withThis));
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error: {f.FullName} " + ex.Message);
+                }
+            }
+
+        }
+
+        private void btnBrowseFolder_Click(object sender, EventArgs e)
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.RestoreDirectory = true;
+
+            if (txtFolderPath.Text != "")
+            {
+                dialog.InitialDirectory = txtFolderPath.Text;
+            }
+            else
+            {
+                dialog.InitialDirectory = "C:";
+            }
+
+
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                txtFolderPath.Text = dialog.FileName;
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmMain_DragDrop(object sender, DragEventArgs e)
+        {
+            List<string> filepaths = new List<string>();
+            foreach (var s in (string[])e.Data.GetData(DataFormats.FileDrop, false))
+            {
+                txtFolderPath.Text = s;
+                if (Directory.Exists(s))
+                {
+                    //Add files from folder
+                    filepaths.AddRange(Directory.GetFiles(s));
+                }
+                else
+                {
+                    //Add filepath
+                    filepaths.Add(s);
+                }
+            }
+        }
+
+        private void frmMain_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+        }
+    }
+}
